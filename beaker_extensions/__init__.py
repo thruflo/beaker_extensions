4147dea48a50b7f8d279c787c406c41e1cc83ea5
James Arthur <thruflo@gmail.com>
2013-10-25 16:25:26 +0100

    dependencies: switch to the lighter pyramid_redis dep.

--- beaker_extensions/__init__.py
+++ beaker_extensions/__init__.py
@@ -1,32 +1,26 @@
+# -*- coding: utf-8 -*-
+
 """Provide a pyramid configuration hook."""
 
-# Lazy import so we can import the package when not using this hook.
-try:
-    from alkey.client import get_redis_client
-except ImportError:
-    get_redis_client = NotImplemented
+import logging
+logger = logging.getLogger(__name__)
 
-def includeme(config, get_redis=None):
+def includeme(config):
     """Patch the session config with a shared redis connection pool."""
     
-    # Compose.
-    if get_redis is None:
-        get_redis = get_redis_client
-    
     # Unpack.
-    settings = config.registry.settings
+    registry = config.registry
+    settings = registry.settings
     
     # Exit if the session backend isn't redis.
     if settings.get('session.type', None) != 'redis':
         return
     
-    # Otherwise make sure we have the dependency.
-    if get_redis_client is NotImplemented:
-        raise ImportError(u'Could not import `alkey.client.get_redis_client`.')
-    
     # Get a configured redis client.
-    redis = get_redis(config)
+    from pyramid_redis.hooks import RedisFactory
+    redis_factory = RedisFactory()
+    redis_client = redis_factory(settings, registry=registry)
     
     # And patch the session settings with its connection pool.
-    settings['session.redis_connection_pool'] = redis.connection_pool
+    settings['session.redis_connection_pool'] = redis_client.connection_pool
 
